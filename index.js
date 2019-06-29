780cc6490930fc8727129d35c14e1dd004564911
Finn Thye <piqueen@gmail.com>
2019-06-29 14:04:34 -0600

    Create function to fillInMadlib

---
+++ index.js
@@ -0,0 +1,12 @@
+function fillInMadlib() {
+  var word1 = document.querySelector('#word-1');
+  var word2 = document.querySelector('#word-2');
+  var word3 = document.querySelector('#word-3');
+  var word4 = document.querySelector('#word-4');
+  var word5 = document.querySelector('#word-5');
+  var word6 = document.querySelector('#word-6');
+  var word7 = document.querySelector('#word-7');
+  var word8 = document.querySelector('#word-8');
+  var word9 = document.querySelector('#word-9');
+  var word10 = document.querySelector('#word-10');
+}
